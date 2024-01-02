e2de9d76ddf10c666a9c8c96ecb4cd2c1ea772c2
Jan Kiszka <jan.kiszka@siemens.com>
2024-01-02 17:40:58 +0100

    scripts: Add automated recipe updates for linux-cip
    Should make life of contributors easier. Just make sure you have run
    
    gpg2 --locate-keys autosigner@kernel.org
    
    before calling this the first time.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

---
+++ scripts/update-linux-cip.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+REPO_ROOT=$(git rev-parse --show-toplevel)
+pushd ${REPO_ROOT}/recipes-kernel/linux >/dev/null
+
+for MAJOR_VERSION in 4.4 4.19 5.10 6.1; do
+	rm -f sha256sums.asc
+	wget -q https://cdn.kernel.org/pub/linux/kernel/projects/cip/${MAJOR_VERSION}/sha256sums.asc
+	if ! gpg2 --verify sha256sums.asc 2>/dev/null; then
+		echo "sha256sums.asc for ${MAJOR_VERSION} invalid!"
+		exit 1
+	fi
+
+	LAST_ENTRY=$(grep "cip[0-9]\+\.tar\.xz" sha256sums.asc | sort -t p -k 3n | tail -1)
+	VERSION=${LAST_ENTRY/*linux-cip-/}
+	VERSION=${VERSION/.tar.xz/}
+	RECIPE_FILE=linux-cip_${VERSION}.bb
+	if [ ! -f ${RECIPE_FILE} ]; then
+		echo "Updating recipe to ${VERSION}"
+		git mv linux-cip_${MAJOR_VERSION}*.bb ${RECIPE_FILE}
+		SHASUM=${LAST_ENTRY/ */}
+		sed -i 's/\(SRC_URI\[sha256sum\] = "\).*/\1'${SHASUM}'"'/ ${RECIPE_FILE}
+		git add ${RECIPE_FILE}
+	fi
+
+	LAST_ENTRY=$(grep "rt[0-9]\+\.tar\.xz" sha256sums.asc | sort -t p -k 3n | tail -1)
+	VERSION=${LAST_ENTRY/*linux-cip-/}
+	VERSION=${VERSION/.tar.xz/}
+	RECIPE_FILE=linux-cip-rt_${VERSION}.bb
+	if [ ! -f ${RECIPE_FILE} ]; then
+		echo "Updating recipe to ${VERSION}"
+		git mv linux-cip-rt_${MAJOR_VERSION}*.bb ${RECIPE_FILE}
+		SHASUM=${LAST_ENTRY/ */}
+		sed -i 's/\(SRC_URI\[sha256sum\] = "\).*/\1'${SHASUM}'"'/ ${RECIPE_FILE}
+		git add ${RECIPE_FILE}
+	fi
+done
+
+rm -f sha256sums.asc
+popd >/dev/null
