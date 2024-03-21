b8fcad62bfdf7b265c01d98a883ccd8682553d2d
Adithya Balakumar <Adithya.Balakumar@toshiba-tsip.com>
2024-03-21 09:58:14 +0530

    reproducible-check-ci: Check reproducibility of wic images
    Currently complete wic images are not being checked for reproducibility.
    This change allows to check reproducility of wic images and if found
    not-reproducible, only then run diffoscope on each partition.
    
    Signed-off-by: Adithya Balakumar <Adithya.Balakumar@toshiba-tsip.com>

--- scripts/repro-tests.sh
+++ scripts/repro-tests.sh
@@ -14,11 +14,12 @@ set -e
 
 usage()
 {
-	echo "usage: repro-tests.sh [--release RELEASE] [--target TARGET] artfacts1 artifacts2"
+	echo "usage: repro-tests.sh [--release RELEASE] [--target TARGET] [--artifact_type ARTIFACT_TYPE] artfacts1 artifacts2"
 	echo ""
 	echo " Optional arguments:"
 	echo "  --release RELEASE: debian distro releases e.g. buster, bullseye, etc. (default: buster)"
 	echo "  --target TARGET: e.g. qemu-amd64, qemu-arm64, qemu-arm (default: qemu-amd64)"
+	echo "  --artifact_type ARTIFACT_TYPE: can be either 'wic-partitions' or 'swu' to selectively run diffoscope on either wic-partitons or swu files"
 	echo ""
 	echo " Mandatory arguments:"
 	echo "  artifacts1 and artifacts2 paths to test the artifacts reproducibility"
@@ -33,6 +34,7 @@ RELEASE="bullseye"
 TARGET="qemu-amd64"
 EXTENSION=""
 DIFFOSCOPE="diffoscope"
+ARTIFACT_TYPE=""
 
 while [ "$1" != "" ]; do
 	case $1 in
@@ -51,6 +53,10 @@ while [ "$1" != "" ]; do
 		-h | --help )
 			usage
 			exit
+			;;
+		--artifact_type )
+			ARTIFACT_TYPE="$2"
+			shift 2
 			;;
 		* )
 			remaining_vars="$remaining_vars $1"
@@ -73,16 +79,24 @@ if [ "${EXTENSION}" = "security" ]; then
 fi
 
 # Define files in the artifacts for checking the reproducibility
-set -- \
-	"${IMAGE_BASE}-${RELEASE}-${TARGET}.swu" \
-	"${IMAGE_BASE}-${RELEASE}-${TARGET}.wic.p0" \
-	"${IMAGE_BASE}-${RELEASE}-${TARGET}.wic.p1" \
-	"${IMAGE_BASE}-${RELEASE}-${TARGET}.wic.p2" \
-	"${IMAGE_BASE}-${RELEASE}-${TARGET}.wic.p3" \
-	"${IMAGE_BASE}-${RELEASE}-${TARGET}.wic.p4" \
-	"${IMAGE_BASE}-${RELEASE}-${TARGET}.wic.p5" \
-	"${IMAGE_BASE}-${RELEASE}-${TARGET}.wic.p6" \
-	"${IMAGE_BASE}-${RELEASE}-${TARGET}.wic.p7"
+swu_files="${IMAGE_BASE}-${RELEASE}-${TARGET}.swu"
+wic_partition_files="
+	${IMAGE_BASE}-${RELEASE}-${TARGET}.wic.p0
+	${IMAGE_BASE}-${RELEASE}-${TARGET}.wic.p1
+	${IMAGE_BASE}-${RELEASE}-${TARGET}.wic.p2
+	${IMAGE_BASE}-${RELEASE}-${TARGET}.wic.p3
+	${IMAGE_BASE}-${RELEASE}-${TARGET}.wic.p4
+	${IMAGE_BASE}-${RELEASE}-${TARGET}.wic.p5
+	${IMAGE_BASE}-${RELEASE}-${TARGET}.wic.p6
+	${IMAGE_BASE}-${RELEASE}-${TARGET}.wic.p7
+"
+set -- $swu_files $wic_partition_files
+
+if [ "${ARTIFACT_TYPE}" = "wic-partitions" ]; then
+	set -- $wic_partition_files
+elif [ "${ARTIFACT_TYPE}" = "swu" ]; then
+	set -- $swu_files
+fi
 
 # compare artifacts
 res=0
