hiromotai7/isar-cip-core

commit 5e2c207abae61e073bc732d0b93ba390891cd0c7

recipes-core/swupdate: Cleanup swupdate.inc
As software loads the bootloader libraries dynamically it is no longer
necessary to set the build loader with BUILD_PROFILES.

Also add PROVIDES for the additional packages swupdate provides.

The profile 'pkg.swupdate.nosigning' is automatically set if signing is
not set to `1`.

Signed-off-by: Quirin Gylstorff <quirin.gylstorff@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

recipes-core/swupdate/swupdate_2021.11-1+debian-gbp.bb
@@ -27,17 +27,6 @@ SRC_URI += "file://0001-debian-Remove-SWUpdate-USB-service-and-Udev-rules.patch 
             file://0004-debian-rules-Add-option-to-disable-webserver.patch \
             file://0005-debian-Add-patch-to-fix-bootloader_env_get-for-EBG.patch"
 
-# end patching for dm-verity based images
-
-# The option: "pkg.swupdate.nosigning" disables the required signing
-# of update binaries
-# DEB_BUILD_PROFILES += "pkg.swupdate.nosigning"
-
-# deactivate hardware compability for simple a/b rootfs update
-DEB_BUILD_PROFILES += "pkg.swupdate.nohwcompat"
-
-# add cross build and deactivate testing for arm based builds
-DEB_BUILD_PROFILES += "cross nocheck"
 # If the luahandler shall be embedded into the swupdate binary
 # include the following lines.
 # DEPENDS += "swupdate-handlers"

recipes-core/swupdate/swupdate.inc
@@ -1,7 +1,7 @@
 #
 # CIP Core, generic profile
 #
-# Copyright (c) Siemens AG, 2021-2022
+# Copyright (c) Siemens AG, 2021-2023
 #
 # Authors:
 #  Quirin Gylstorff <quirin.gylstorff@siemens.com>
@@ -13,15 +13,10 @@ HOMEPAGE= "https://github.com/sbabic/swupdate"
 LICENSE = "GPL-2.0"
 LIC_FILES_CHKSUM = "file://${LAYERDIR_isar}/licenses/COPYING.GPLv2;md5=751419260aa954499f7abaabaa882bbe"
 
-def get_bootloader_build_profile(d):
-    bootloader = d.getVar("SWUPDATE_BOOTLOADER") or ""
-    if bootloader == "efibootguard":
-        return "pkg.swupdate.efibootguard"
-    if bootloader == "u-boot":
-        return "pkg.swupdate.uboot"
-    return ""
+PROVIDES += "libswupdate-dev"
+PROVIDES += "libswupdate0.1"
+PROVIDES += "lua-swupdate"
 
-DEB_BUILD_PROFILES += "${@get_bootloader_build_profile(d)}"
 
 def get_bootloader_dependencies(d):
     bootloader = d.getVar("SWUPDATE_BOOTLOADER") or ""
@@ -36,6 +31,14 @@ def get_bootloader_dependencies(d):
 
 DEPENDS += "${@get_bootloader_dependencies(d)}"
 DEPENDS += "${@bb.utils.contains('DEB_BUILD_PROFILES', 'mtd', 'mtd-utils', '', d)}"
+
+DEB_BUILD_PROFILES += "${@'pkg.swupdate.nosigning' if not d.getVar('SWU_SIGNED') == '1' else ''}"
+
+# deactivate hardware compability for simple a/b rootfs update
+DEB_BUILD_PROFILES += "pkg.swupdate.nohwcompat"
+
+# add cross build and deactivate testing for arm based builds
+DEB_BUILD_PROFILES += "cross nocheck"
 
 python do_check_bootloader () {
     bootloader = d.getVar("SWUPDATE_BOOTLOADER") or "None"

recipes-core/swupdate/swupdate_2023.05.bb
@@ -31,13 +31,6 @@ SRC_URI += "file://0001-d-rules-Add-seperate-build_profile-option-for-delta-.pat
             file://0003-d-rules-Add-option-to-enable-suricatta_wfx.patch"
 SRC_URI:append:bullseye = " file://0004-d-swupdate-www.install-Fix-path-for-debian-bullseye.patch"
 
-# The option: "pkg.swupdate.nosigning" disables the required signing
-# of update binaries
-# DEB_BUILD_PROFILES += "pkg.swupdate.nosigning"
-
-# deactivate hardware compability for simple a/b rootfs update
-DEB_BUILD_PROFILES += "pkg.swupdate.nohwcompat"
-
 # suricatta wfx requires suricatta lua and the dependency
 # is not set automatically
 DEB_BUILD_PROFILES += "pkg.swupdate.suricattalua"
@@ -47,9 +40,6 @@ DEB_BUILD_PROFILES += "pkg.swupdate.suricattawfx"
 # Disable cross for arm and arm64 on bullseye
 # with cross compile we have a unsat-dependency to dh-nodejs on arm/arm64
 ISAR_CROSS_COMPILE:bullseye = "0"
-
-# add cross build and deactivate testing for arm based builds
-DEB_BUILD_PROFILES += "cross nocheck"
 
 # use backport build profile for bullseye
 DEB_BUILD_PROFILES:append:bullseye = " pkg.swupdate.bpo"

commit 9f9cb8809ae20314a3214ac0bd2a7f6d86a6e783

fix: swupdate_2021.11 with efibootguard
This fixes a regression introduced with
5e2c207abae61e073bc732d0b93ba390891cd0c7 by readding the bootloader
selection with DEB_BUILD_PROFILES for SWUpdate 2021.11.

SWUpdate 2021.11 selects the bootloader at  compile time instead
of runtime. This fixes the following error message during installing
a binary update:

 [INFO ] : SWUPDATE running :  Installation in progress
 [TRACE] : SWUPDATE running :  [install_single_image] : Found installer
 for stream cip-core-image-cip-core-bookworm-qemu-amd64.squashfs roundrobin
 [TRACE] : SWUPDATE running :  [notify_helper] : Round Robin Handler: No
 compiled-in config found, trying /etc/swupdate.handler.ini
 [ERROR] : SWUPDATE failed [0] ERROR : Round Robin Handler: Error getting
 key 'kernelfile' from bootloader environment.

Signed-off-by: Quirin Gylstorff <quirin.gylstorff@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

recipes-core/swupdate/swupdate_2021.11-1+debian-gbp.bb
@@ -34,6 +34,16 @@ SRC_URI += "file://0001-debian-Remove-SWUpdate-USB-service-and-Udev-rules.patch 
 # GBP_DEPENDS += "swupdate-handlers"
 # DEB_BUILD_PROFILES += "pkg.swupdate.embeddedlua"
 
+def get_bootloader_build_profile(d):
+    bootloader = d.getVar("SWUPDATE_BOOTLOADER") or ""
+    if bootloader == "efibootguard":
+        return "pkg.swupdate.efibootguard"
+    if bootloader == "u-boot":
+        return "pkg.swupdate.uboot"
+    return ""
+
+DEB_BUILD_PROFILES += "${@get_bootloader_build_profile(d)}"
+
 # modify for debian buster build
 SRC_URI:append = " file://0006-debian-prepare-build-for-isar-debian-buster.patch"
 