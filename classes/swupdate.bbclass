hiromotai7/isar-cip-core

commit ad089e66275a6ab225a4c5ad28d1d8976c414ae7

make name, version and hw-compat available via bb
This patch adds bitbake variables to control the corresponding entries
in the sw-description file. The following variables are added:

- SWU_NAME: software->name
- SWU_VERSION: software->version
- SWU_HW_COMPAT: software->compatibility (only if non empty)

By providing these variables, downstream users more often can use the
default sw-description file. This makes it easier to maintain and evolve
the swupdate infrastructure without breaking downstream use-cases.
It also brings as closer to the way swupdate is integrated into yocto.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

classes/swupdate.bbclass
@@ -17,6 +17,10 @@ SWU_ROOTFS_NAME ?= "${IMAGE_FULLNAME}"
 # compression type as defined by swupdate (zlib or zstd)
 SWU_COMPRESSION_TYPE ?= "zlib"
 SWU_ROOTFS_PARTITION_NAME ?= "${SWU_ROOTFS_NAME}.${SWU_ROOTFS_TYPE}.${@get_swu_compression_type(d)}"
+SWU_VERSION ?= "0.2"
+SWU_NAME ?= "cip software update"
+# space separated list of supported hw. Leave empty to leave out
+SWU_HW_COMPAT ?= ""
 
 SWU_IMAGE_FILE ?= "${DEPLOY_DIR_IMAGE}/${IMAGE_FULLNAME}.swu"
 SWU_DESCRIPTION_FILE ?= "sw-description"
@@ -37,7 +41,22 @@ IMAGE_TEMPLATE_VARS:swu = " \
     TARGET_IMAGE_UUID \
     ABROOTFS_PART_UUID_A \
     ABROOTFS_PART_UUID_B \
-    SWU_COMPRESSION_TYPE"
+    SWU_COMPRESSION_TYPE \
+    SWU_HW_COMPAT_NODE \
+    SWU_VERSION \
+    SWU_NAME"
+
+python(){
+    # create SWU_HW_COMPAT_NODE based on list of supported hw
+    hw_compat = d.getVar('SWU_HW_COMPAT')
+    if hw_compat:
+        hw_entries = ', '. join(['"' + h + '"' for h in hw_compat.split()])
+        d.setVar('SWU_HW_COMPAT_NODE',
+            'hardware-compatibility: [ ' + hw_entries +' ];')
+    else:
+        d.setVar('SWU_HW_COMPAT_NODE', '')
+}
+
 
 # convert between swupdate compressor name and imagetype extension
 def get_swu_compression_type(d):

recipes-core/images/swu/sw-description.tmpl
@@ -10,8 +10,9 @@
 #
 software =
 {
-    version = "0.2";
-    name = "cip software update";
+    version = "${SWU_VERSION}";
+    name = "${SWU_NAME}";
+    ${SWU_HW_COMPAT_NODE}
     images: ({
             filename = "${SWU_ROOTFS_PARTITION_NAME}";
             device = "C:BOOT0:linux.efi->${ABROOTFS_PART_UUID_A},C:BOOT1:linux.efi->${ABROOTFS_PART_UUID_B}";

commit 692ca5a5277cf60202fdbde2e92548937bf64f92

swupdate: add vardep on SWU_HW_COMPAT
The generation of the sw-description file depends on this variable, but
as it is read in a python function, the dependency to its value cannot
be detected by bitbake. We now explicitly add it to the vardeps of the
prefunc, as the transitively called function cannot have vardeps.

Fixes: ad089e6 ("make name, version and hw-compat available via bb")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

classes/swupdate.bbclass
@@ -74,6 +74,7 @@ IMAGE_TEMPLATE_VARS:swu = " \
 # TARGET_IMAGE_UUID needs to be generated before completing the template
 addtask transform_template after do_generate_image_uuid
 
+do_extend_sw_description[vardeps] += "SWU_HW_COMPAT"
 python do_extend_sw_description() {
     cmds = d.getVar("SWU_EXTEND_SW_DESCRIPTION")
     if cmds is None or not cmds.strip():