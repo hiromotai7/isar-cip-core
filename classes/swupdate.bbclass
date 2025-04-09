hiromotai7/isar-cip-core

commit 2f306b74b19a82a2c90c08cb16500dee68a2bc1a

swupdate.bbclass: Add check if a swu file is greater than 4GB
4GB is the size limit of a file in a cpio archive. cpio prints
the following message but returns zero in that case.

```
value file size 10 out of allowed range 0..4294967295
```

The sum of files in the archive can be greater than 4GB.

This fixes Issue #125.

Signed-off-by: Quirin Gylstorff <quirin.gylstorff@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

classes/swupdate.bbclass
@@ -247,7 +247,17 @@ IMAGE_CMD:swu() {
                 fi
                 cpio_files="$cpio_files $signature_file"
             fi
-
+            # check if swu_files are less than 4GBytes.
+            # This avoids the limit of cpio
+            for swu_file in $swu_files; do
+                file_size =$(stat -c %s "$swu_file")
+                if [ "$file_size" -ge 4294967295 ] ; then
+                    echo "The size of '$swu_file': '$file_size' is greater" \
+                          "than the limit of the swu format of 4294967295" \
+                          "Bytes per file" 1>&2
+                    exit 1
+                fi
+            done
             # sw-description must be first file in *.swu
             for cpio_file in $cpio_files $swu_files; do
                 if [ -f "$cpio_file" ]; then

commit 471b159abce60e91e58da80b8ae759e7da0033f5

swupdate.bbclass: fix forgotten whitespace in assignment
Fixes `file_size: command not found` error in do_image_swu.

Fixes: 2f306b7 ("swupdate.bbclass: Add check if a swu file is greater than 4GB")
Signed-off-by: Gokhan Cetin <gokhan.cetin@siemens.com>
Reviewed-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

classes/swupdate.bbclass
@@ -251,7 +251,7 @@ IMAGE_CMD:swu() {
             # check if swu_files are less than 4GBytes.
             # This avoids the limit of cpio
             for swu_file in $swu_files; do
-                file_size =$(stat -c %s "$swu_file")
+                file_size=$(stat -c %s "$swu_file")
                 if [ "$file_size" -ge 4294967295 ] ; then
                     echo "The size of '$swu_file': '$file_size' is greater" \
                           "than the limit of the swu format of 4294967295" \